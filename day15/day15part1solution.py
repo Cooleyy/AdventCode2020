ccc5039dcfd5e7e7ea9b28de051175892c469e55
Cooley <jcool841@yahoo.com>
2021-05-02 20:25:38 +0100

    day 15 solutions

---
+++ day15/day15part1solution.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import sys
+import re
+from copy import deepcopy
+
+def main():
+    puzzleInputFile = open(sys.argv[1], 'r')
+    puzzleInput = puzzleInputFile.read().split(",")
+
+    lastSpoken = {}
+
+    for i, num in enumerate(puzzleInput, 1):
+        lastSpoken[int(num)] = i
+
+    nextNum = 0
+    turn = len(puzzleInput) + 1
+    lastNum = 0
+
+    while(turn < 2020):
+        if nextNum in lastSpoken:
+            lastNum = nextNum
+            nextNum = turn - lastSpoken[nextNum]
+            lastSpoken[lastNum] = turn
+        else:
+            lastSpoken[nextNum] = turn
+            nextNum = 0
+        turn += 1
+    
+    print(nextNum)
+
+if __name__ == "__main__":
+    main()
